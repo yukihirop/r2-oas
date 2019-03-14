3904aef42b3c1fe4c91113aab5733e41760ba39c
yukihirop <te108186@gmail.com>
2019-03-14 21:52:42 +0900

    bundle gem routes_to_swagger_docs -t

---
+++ lib/routes_to_swagger_docs.rb
@@ -0,0 +1,6 @@
+require "routes_to_swagger_docs/version"
+
+module RoutesToSwaggerDocs
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/routes_to_swagger_docs/version.rb
@@ -0,0 +1,3 @@
+module RoutesToSwaggerDocs
+  VERSION = "0.1.0"
+end
