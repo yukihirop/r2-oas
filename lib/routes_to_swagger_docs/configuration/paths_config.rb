43f725f7eb91655f21f4492543d3df6b553d03ff
yukihirop <te108186@gmail.com>
2019-06-17 11:42:38 +0900

    Fix PathsConfig#create_dot_paths(private)

--- lib/routes_to_swagger_docs/configuration/paths_config.rb
+++ lib/routes_to_swagger_docs/configuration/paths_config.rb
@@ -20,7 +20,7 @@ module RoutesToSwaggerDocs
         abs_root_path = File.expand_path(@root_dir_path)
 
         FileUtils.mkdir_p(abs_root_path) unless FileTest.exists?(abs_root_path)
-        File.write(abs_dot_paths_path, "") unless FileTest.exists?(abs_paths_path)
+        File.write(abs_paths_path, "") unless FileTest.exists?(abs_paths_path)
       end
     end
   end
