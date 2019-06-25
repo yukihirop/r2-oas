2f03c448e802da2029249fa4329e8b97fe1b5cb6
yukihirop <te108186@gmail.com>
2019-06-26 00:13:20 +0900

    [Refactor] Use PathItemFileManager to fetch many file paths

--- lib/routes_to_swagger_docs/schema/generator/base_generator.rb
+++ lib/routes_to_swagger_docs/schema/generator/base_generator.rb
@@ -3,6 +3,7 @@ require_relative '../v3/openapi_object'
 require_relative '../base'
 require_relative '../squeezer'
 require_relative '../../shared/all'
+require_relative '../manager/file/path_item_file_manager'
 
 module RoutesToSwaggerDocs
   module Schema
@@ -69,67 +70,10 @@ module RoutesToSwaggerDocs
 
       def components_file_paths
         many_paths_file_paths.each_with_object([]) do |unit_paths_path, result|
-          components_file_paths_at_path = components_file_paths_for_path(unit_paths_path)
+          file_manager = PathItemFileManager.new(unit_paths_path, :full)
+          components_file_paths_at_path = file_manager.descendants_ref_paths
           result.push(*components_file_paths_at_path)
         end.uniq
-      end
-
-      def components_file_paths_for_path(path)
-        yaml = YAML.load_file(path)
-        
-        components_paths = []
-        deep_search_component_file_recursive(yaml, "$ref") do |component_paths|
-          components_paths.push(*component_paths)
-        end
-
-        components_paths = components_paths.uniq
-        components_paths.each_with_object([]) do |component_path, result|
-          result.push(File.expand_path(component_path))
-        end
-      end
-
-      def deep_search_component_file_recursive(yaml, target, &block)
-        if yaml.is_a?(Hash)
-          yaml.each do |key, value|
-            process_deep_search_component_file_recursive(key, value, target, &block)
-          end
-        # Support allOf/oneOf/anyOf 
-        elsif yaml.is_a?(Array)
-          yaml.each do |el|
-            if el.is_a?(Hash)
-              el.each do |key, value|
-                process_deep_search_component_file_recursive(key, value, target, &block)
-              end
-            end
-          end
-        end
-      end
-
-      def process_deep_search_component_file_recursive(key, value, target, &block)
-        component_info = value
-
-        if key.eql? target
-          relative_component_path_data = component_info.gsub("#/","").split("/")
-          relative_component_path = relative_component_path_data.each.with_index.inject("") do |base,(value, index)|
-            if index == relative_component_path_data.size - 1
-              value = value.to_s.gsub("_", "/").underscore
-            end
-            "#{base}/#{value}"
-          end
-
-          component_path = "#{schema_save_dir_path}#{relative_component_path}.yml"
-          component_data = YAML.load_file(component_path)
-
-          children_components_paths = []
-          deep_search_component_file_recursive(component_data, target) do |children_components_path|
-            children_components_paths.push(*children_components_path)
-          end
-
-          components_paths = [ component_path ] + children_components_paths
-          yield components_paths if block_given?
-        else
-          deep_search_component_file_recursive(component_info, target, &block)
-        end
       end
     end
   end
