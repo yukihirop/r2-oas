5b5eb901c22f1fc0ccb703067b3a2d4726aba9e9
yukihirop <te108186@gmail.com>
2019-10-04 21:37:30 +0900

    Support all path item object fixed fields when analyze

--- lib/routes_to_swagger_docs/schema/analyzer/path_analyzer.rb
+++ lib/routes_to_swagger_docs/schema/analyzer/path_analyzer.rb
@@ -7,6 +7,9 @@ require_relative '../manager/file/path_item_file_manager'
 module RoutesToSwaggerDocs
   module Schema
     class PathAnalyzer < BaseAnalyzer
+      VERB = %w(get put post delete options head patch trace)
+      NOT_VERB = %w($ref summary description servers parameters)
+
       def analyze_docs
         sorted_schema = deep_sort(@after_schema_data, 'paths')
         edited_paths_schema = sorted_schema['paths']
@@ -22,27 +25,56 @@ module RoutesToSwaggerDocs
 
       def unit_paths_data_group_by_tags(unit_paths_data)
         unit_paths_data.each_with_object({}) do |(path, data_when_path), result|
-          data_when_path.each do |verb, data_when_verb|
-
-            if !data_when_verb.is_a?(Array) && data_when_verb.has_key?('tags')
-              tag_name = data_when_verb['tags'].first
+          data_when_path.each do |verb_or_not, data_when_verb_or_not|
+            if VERB.include?(verb_or_not)
+              result = group_by_tags_when_verb(verb_or_not, data_when_verb_or_not, path, result)
+            elsif NOT_VERB.include?(verb_or_not)
+              result = group_by_tags_when_not_verb(verb_or_not, data_when_verb_or_not, path, result)
             else
-              tag_name = 'unknown'
-              data_when_verb.deep_merge!({
-                'tags' => [ tag_name ]
-              })
+              raise "Do not support path item object keys: #{verb_or_not}"
             end
-
-            result[tag_name] ||= {}
-            result[tag_name].deep_merge!(
-              'paths' => {
-                path => {
-                  verb => data_when_verb
-                }
-              }
-            )
           end
         end
+      end
+
+      def group_by_tags_when_verb(verb, data_when_verb, path, result)
+        if !data_when_verb.is_a?(Array) && data_when_verb.has_key?('tags')
+          tag_name = data_when_verb['tags'].first
+        else
+          tag_name = 'unknown'
+          data_when_verb.deep_merge!({
+            'tags' => [ tag_name ]
+          })
+        end
+
+        result[tag_name] ||= {}
+        result[tag_name].deep_merge!(
+          'paths' => {
+            path => {
+              verb => data_when_verb
+            }
+          }
+        )
+        result
+      end
+
+      # The following pseudo tag names are after reservation
+      # ・$ref
+      # ・summary
+      # ・description
+      # ・servers
+      # ・parameters
+      def group_by_tags_when_not_verb(not_verb, data_when_not_verb, path, result)
+        tag_name = not_verb
+        result[tag_name] ||= {}
+        result[tag_name].deep_merge!(
+          'paths' => {
+            path => {
+              not_verb => data_when_not_verb
+            }
+          }
+        )
+        result
       end
 
       def save_each_tags(unit_paths_data)
