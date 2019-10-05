63517bf5c1ad4ef8ab1ff845767a1518fe3c8865
yukihirop <te108186@gmail.com>
2019-10-06 00:02:16 +0900

    Delete components squeezer　Unnecessary

--- lib/routes_to_swagger_docs/schema/squeezer.rb
+++ lib/routes_to_swagger_docs/schema/squeezer.rb
@@ -3,22 +3,19 @@
 require_relative 'squeezer/base_squeezer'
 require_relative 'squeezer/tag_squeezer'
 require_relative 'squeezer/path_squeezer'
-require_relative 'squeezer/components_squeezer'
 
 module RoutesToSwaggerDocs
   module Schema
     class Squeezer < BaseSqueezer
       def squeeze_docs
-        except_paths_schema = @schema_data.except('paths', 'tags', 'components')
+        except_paths_schema = @schema_data.except('paths', 'tags')
 
         path_squeezer = PathSqueezer.new(@schema_data, many_paths_file_paths: many_paths_file_paths)
         tag_squeezer = TagSqueezer.new(@schema_data, many_paths_file_paths: many_paths_file_paths)
-        components_squeezer = ComponentsSqueezer.new(@schema_data, many_paths_file_paths: many_paths_file_paths)
 
         slice_schemas = [
           tag_squeezer.squeeze_docs,
-          path_squeezer.squeeze_docs,
-          components_squeezer.squeeze_docs
+          path_squeezer.squeeze_docs
         ]
         slice_schemas.each_with_object(except_paths_schema) { |slice_schema, result| result.deep_merge!(slice_schema) }
       end
