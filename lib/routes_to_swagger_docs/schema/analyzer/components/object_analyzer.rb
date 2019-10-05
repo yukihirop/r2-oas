2ffd832b2f8030358966c103e6ce1c38929b5582
yukihirop <te108186@gmail.com>
2019-10-06 01:37:36 +0900

    Unified into Components::ObjectAnalyzer

--- lib/routes_to_swagger_docs/schema/analyzer/components/object_analyzer.rb
+++ lib/routes_to_swagger_docs/schema/analyzer/components/object_analyzer.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 require_relative '../base_analyzer'
 require_relative '../../manager/file/components_file_manager'
 require_relative '../../manager/diff/components_diff_manager'
@@ -8,11 +6,17 @@ require_relative '../../manager/diff/components_diff_manager'
 module RoutesToSwaggerDocs
   module Schema
     module Components
-      class SchemasAnalyzer < BaseAnalyzer
+      class ObjectAnalyzer < BaseAnalyzer
+        def initialize(before_schema_data, after_schema_data, options = {})
+          super(before_schema_data, after_schema_data, options.except(:middle_category))
+          @major_category = 'components'
+          @middle_category = options[:middle_category]
+        end
+
         def analyze_docs
-          diff_manager = ComponentsDiffManager.new(@before_schema_data, @after_schema_data, { middle_category: 'schemas' })
+          diff_manager = ComponentsDiffManager.new(@before_schema_data, @after_schema_data, { middle_category: @middle_category })
           diff_manager.process_by_using_diff_data do |schema_name, is_removed, is_added, is_leftovers, after_edited_data|
-            file_manager = ComponentsFileManager.build("#/components/schemas/#{schema_name}", :ref)
+            file_manager = ComponentsFileManager.build("#/#{@major_category}/#{@middle_category}/#{schema_name}", :ref)
             save_file_path = file_manager.save_file_path
 
             if is_removed && !is_added && !is_leftovers
--- lib/routes_to_swagger_docs/schema/analyzer/components_analyzer.rb
+++ lib/routes_to_swagger_docs/schema/analyzer/components_analyzer.rb
@@ -1,39 +1,29 @@
 # frozen_string_literal: true
 
 require_relative 'base_analyzer'
-require_relative 'components/schemas_analyzer'
-require_relative 'components/request_bodies_analyzer'
-require_relative 'components/security_schemes_analyzer'
-require_relative 'components/parameters_analyzer'
+require_relative 'components/object_analyzer'
 
 # Scope Rails
 module RoutesToSwaggerDocs
   module Schema
     class ComponentsAnalyzer < BaseAnalyzer
+      COMPONENTS_OBJECTS = %i(schemas requestBodies securitySchemes parameters)
+
       def initialize(before_schema_data, after_schema_data, options = {})
         super
-        @components_schemas_analyzer          = Components::SchemasAnalyzer.new(before_schema_data, after_schema_data, options)
-        @components_request_bodies_analyzer   = Components::RequestBodiesAnalyzer.new(before_schema_data, after_schema_data, options)
-        @components_security_schemas_analyzer = Components::SecuritySchemesAnalyzer.new(before_schema_data, after_schema_data, options)
-        @components_parameters_analyzer       = Components::ParametersAnalyzer.new(before_schema_data, after_schema_data, options)
-      end
+        @options = options
+      end      
 
       def analyze_docs
-        logger.info '[Analyze Swagger file (components/schemas)] start'
-        @components_schemas_analyzer.analyze_docs
-        logger.info '[Analyze Swagger file (components/schemas)] end'
-
-        logger.info '[Analyze Swagger file (components/requestBodies)] start'
-        @components_request_bodies_analyzer.analyze_docs
-        logger.info '[Analyze Swagger file (components/requestBodies)] end'
-
-        logger.info '[Analyze Swagger file (components/securitySchemas)] start'
-        @components_security_schemas_analyzer.analyze_docs
-        logger.info '[Analyze Swagger file (components/securitySchemas)] end'
-
-        logger.info '[Analyze Swagger file (components/parameters)] start'
-        @components_parameters_analyzer.analyze_docs
-        logger.info '[Analyze Swagger file (components/parameters)] end'
+        COMPONENTS_OBJECTS.each do |object_name|
+          logger.info "[Analyze Swagger file (components/#{object_name})] start"
+          Components::ObjectAnalyzer.new(
+            @before_schema_data, 
+            @after_schema_data, 
+            @options.merge({ middle_category: object_name.to_s })
+          ).analyze_docs
+          logger.info "[Analyze Swagger file (components/#{object_name})] end"
+        end
       end
     end
   end
