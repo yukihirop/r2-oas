a17c153c9160b03bb0fe6021745535353fb5d921
yukihirop <te108186@gmail.com>
2019-10-06 20:35:55 +0900

    Avoid path_item_file_manager version dependencies

---
+++ lib/routes_to_swagger_docs/schema/manager/file/path_item_file_manager.rb
@@ -0,0 +1,22 @@
+module RoutesToSwaggerDocs
+  module Schema
+    class PathItemFileManager
+      extend Forwardable
+        
+      def_delegators :@manager, :skip_save?, :descendants_paths, :descendants_ref_paths
+
+      def initialize(path, path_type = :ref)
+        case ::RoutesToSwaggerDocs.version
+        when :v3
+          @manager = V3::PathItemFileManager.new(path, path_type)
+        else
+          raise "Do not support version: #{::RoutesToSwaggerDocs.version}"
+        end
+      end
+
+      class << self
+        alias :build :new
+      end
+    end
+  end
+end
