b238c7affc1f767ccb5f0ff342d4df89f3ab3ece
yukihirop <te108186@gmail.com>
2019-10-12 18:02:28 +0900

    To make components merge after paths

--- lib/routes_to_swagger_docs/schema/v3/squeezer.rb
+++ lib/routes_to_swagger_docs/schema/v3/squeezer.rb
@@ -9,14 +9,17 @@ module RoutesToSwaggerDocs
     module V3
       class Squeezer < BaseSqueezer
         def squeeze_docs
-          except_paths_schema = @schema_data.except('paths', 'tags')
+          except_paths_schema = @schema_data.except('paths', 'tags', 'components')
+          components_schemas = @schema_data.slice('components')
 
           path_squeezer = PathSqueezer.new(@schema_data, many_paths_file_paths: many_paths_file_paths)
           tag_squeezer = TagSqueezer.new(@schema_data, many_paths_file_paths: many_paths_file_paths)
 
+          # To make components merge after paths
           slice_schemas = [
             tag_squeezer.squeeze_docs,
-            path_squeezer.squeeze_docs
+            path_squeezer.squeeze_docs,
+            components_schemas
           ]
           slice_schemas.each_with_object(except_paths_schema) { |slice_schema, result| result.deep_merge!(slice_schema) }
         end
