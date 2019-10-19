87a580059541744935167be1dce8440ef50ec0b8
yukihirop <te108186@gmail.com>
2019-10-19 15:54:22 +0900

    Add test about paths_config

--- spec/support/helpers/create_helper.rb
+++ spec/support/helpers/create_helper.rb
@@ -43,4 +43,110 @@ module CreateHelper
     schema_data = YAML.load_file(swagger_file_path(:yml))
     File.write(doc_save_file_path, schema_data.to_yaml)
   end
+
+  def prepare_standard_files
+    # Task
+    create_components_schemas_file('api/v1/task.yml', {
+        "components" => {
+          "schemas" => {
+            "Api_V1_Task" => {
+              "type" => "object",
+              "properties"=> {
+                "id" => {
+                  "type" => "integer",
+                  "format" => "int64"
+                }
+              }
+            }
+          }
+        }
+      }.to_yaml)
+      create_components_schemas_file('api/v1/task/rb.yml', {
+        "components" => {
+          "schemas" => {
+            "Api_V1_Task_RB" => {
+              "type" => "object",
+              "properties"=> {
+                "id" => {
+                  "type" => "integer",
+                  "format" => "int64"
+                }
+              }
+            }
+          }
+        }
+      }.to_yaml)
+      create_components_request_bodies_file('api/v1/task/rb.yml', {
+        "components" => {
+          "requestBodies" => {
+            "Api_V1_Task_RB" => {
+              "content" => {
+                "application/json" => {
+                  "schema" => {
+                    "$ref" => "#/components/schemas/Api_V1_Task_RB"
+                  }
+                }
+              }
+            }
+          }
+        }
+      }.to_yaml)
+      create_paths_file('api/v1/task.yml',{
+        "paths" => {
+          "post" => {
+            "summary" => "api/v1/task summary",
+            "description" => "api/v1/task description",
+            "responses" => {
+              "201" => {
+                "content" => {
+                  "application/json" => {
+                    "schema" => {
+                      "$ref" => "#/components/schemas/Api_V1_Task"
+                    }
+                  }
+                }
+              }
+            },
+            "requestBody" => {
+              "$ref" => "#/components/requestBodies/Api_V1_Task_RB"
+            }
+          }
+        }
+      }.to_yaml)
+      # User
+      create_components_schemas_file('/api/v1/user.yml', {
+        "components" => {
+          "schemas" => {
+            "Api_V1_User" => {
+              "type" => "object",
+              "properties"=> {
+                "id" => {
+                  "type" => "integer",
+                  "format" => "int64"
+                }
+              }
+            }
+          }
+        }
+      }.to_yaml)
+      create_paths_file('api/v1/user.yml',{
+        "paths" => {
+          "get" => {
+            "summary" => "api/v1/user summary",
+            "description" => "api/v1/user description",
+            "responses" => {
+              "200" => {
+                "content" => {
+                  "application/json" => {
+                    "schema" => {
+                      "$ref" => "#/components/schemas/Api_V1_User"
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }.to_yaml)
+  end
 end
